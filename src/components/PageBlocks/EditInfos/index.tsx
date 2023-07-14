e9427d01fcdc8691ff81e41573a905782e60e5d7
Pedro Barreto <pedro.barreto@take.net>
2023-07-14 20:15:08 -0300

    style: change style of `EditInfo` component

--- src/components/PageBlocks/EditInfos/index.tsx
+++ src/components/PageBlocks/EditInfos/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import {
   Box,
+  Collapse,
   Divider,
   Drawer,
   IconButton,
@@ -9,12 +10,15 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  TextField,
   Tooltip,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
-import { Add } from "@mui/icons-material";
+import { Add, StarBorder } from "@mui/icons-material";
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
 
 export default function EditInfos({
   open: state,
@@ -22,41 +26,66 @@ export default function EditInfos({
   addBlock,
   closeDrawer,
 }: any) {
+  const [open, setOpen] = useState(true);
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   const list = (anchor: any) => (
-    <>
+    <div style={{ width: '80%', margin: '0 auto' }}>
       <Box
-        sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+        sx={{ width: 'auto', padding: "20px 40px" }}
         role="presentation"
         onKeyDown={toggleDrawer(anchor, true)}
       >
         <List>
-          {["Tag", "Cor", "Formato"].map((text, index) => (
+          {[{ title: "Tag", description: "[SELECT]" }, { title: "Cor de fundo", description: "#555" }, { title: "Cor do texto", description: "#555" }].map(({ title, description }, index) => (
             <>
-              <ListItem key={text} disablePadding>
+              {/* <ListItem key={title} disablePadding>
                 <ListItemButton>
                   <ListItemIcon>
                     {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                   </ListItemIcon>
-                  <ListItemText primary={text} />
+                  <ListItemText primary={title} />
                 </ListItemButton>
-              </ListItem>
+              </ListItem> */}
+              <TextField  sx={{ width: '100%' }} id="outlined-basic" label={title} variant="filled" placeholder={description} />
               <Divider />
             </>
           ))}
         </List>
+
+        <ListItemButton onClick={handleClick}>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Formato" />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 4 }}>
+              <ListItemIcon>
+                <StarBorder />
+              </ListItemIcon>
+              <ListItemText primary="Starred" />
+            </ListItemButton>
+          </List>
+        </Collapse>
+        <Divider />
       </Box>
+
       <Tooltip title="Adicionar mais blocos">
-        <IconButton sx={{ "&:hover": { backgroundColor: "#111", color: "#fff" }, borderRadius: "0" }} onClick={() => { addBlock(), closeDrawer() } }>
-          <Add color="success" />
+        <IconButton sx={{ "&:hover": { backgroundColor: "#11a811", color: "#333" }, borderRadius: "6px", width: '100%', marginBottom: '16px' }} onClick={() => { addBlock(), closeDrawer() } }>
           Salvar
         </IconButton>
       </Tooltip>
-    </>
+    </div>
   );
 
   return (
     <Drawer
-      anchor={"bottom"}
+      anchor="bottom"
       open={state["bottom"]}
       onClose={toggleDrawer("bottom", false)}
     >
--- src/components/PageBlocks/index.tsx
+++ src/components/PageBlocks/index.tsx
@@ -1,46 +1,53 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, KeyboardEvent, MouseEvent } from "react";
 import ButtonAddBlock from "../ButtonAddBlock";
 import ListBlocks from "./ListBlocks";
 import EditInfos from "./EditInfos";
 
-type Anchor = 'bottom';
+type Anchor = "bottom";
 
 export default function PageBlocks({ blocks }: any) {
-  const [listBocks, setListBocks] = useState([{ name: "1" }, { name: "2" }]) as any;
-  const [state, setState] = React.useState({ bottom: false});
+  const [listBocks, setListBocks] = useState([
+    { name: "1" },
+    { name: "2" },
+  ]) as any;
+  const [state, setState] = React.useState({ bottom: false });
 
   const openDrawer = () => {
-    setState({ bottom: true })
+    setState({ bottom: true });
   };
 
   const closeDrawer = () => {
-    setState({ bottom: false })
+    setState({ bottom: false });
   };
 
-  const addBlock = () => {    
+  const addBlock = () => {
     setListBocks([...listBocks, { name: `${listBocks.length + 1}` }]);
-  }
+  };
 
   const toggleDrawer =
-  (anchor: Anchor, open: boolean) =>
-  (event: React.KeyboardEvent | React.MouseEvent) => {
-    if (
-      event.type === 'keydown' &&
-      ((event as React.KeyboardEvent).key === 'Tab' ||
-        (event as React.KeyboardEvent).key === 'Shift')
-    ) {
-      return;
-    }
+    (anchor: Anchor, open: boolean) => (event: KeyboardEvent | MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as KeyboardEvent).key === "Tab" ||
+          (event as KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
 
-    setState({ ...state, [anchor]: open });
-  };
+      setState({ ...state, [anchor]: open });
+    };
 
   return (
-  <>
-    <ButtonAddBlock openDrawer={openDrawer} />
-    <ListBlocks blocks={listBocks} />
-    <EditInfos open={state} toggleDrawer={toggleDrawer} addBlock={addBlock} closeDrawer={closeDrawer} />
-  </>
+    <>
+      <ButtonAddBlock openDrawer={openDrawer} />
+      <ListBlocks blocks={listBocks} />
+      <EditInfos
+        open={state}
+        toggleDrawer={toggleDrawer}
+        addBlock={addBlock}
+        closeDrawer={closeDrawer}
+      />
+    </>
   );
 }
